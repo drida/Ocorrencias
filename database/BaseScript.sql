a018aea328ad771bfbd3a065ea43ff0e38e2df1a
DriDa - Computer Engineering <drida@drida.com.br>
2021-11-20 23:35:05 -0300

    Update BaseScript.sql

--- database/BaseScript.sql
+++ database/BaseScript.sql
@@ -1,6 +1,8 @@
 DROP TABLE IF EXISTS public.empresa CASCADE;
 DROP TABLE IF EXISTS public.equipe CASCADE;
 DROP TABLE IF EXISTS public.status CASCADE;
+DROP TABLE IF EXISTS public.areaimpactada CASCADE;
+DROP TABLE IF EXISTS public.etapaimpactada CASCADE;
 DROP TABLE IF EXISTS public.usuario CASCADE;
 DROP TABLE IF EXISTS public.tipoocorrencia CASCADE;
 DROP TABLE IF EXISTS public.sistemas CASCADE ;
@@ -112,6 +114,88 @@ AS SELECT o.id,
      JOIN etapaimpactada eti ON eti.id = o.idetapaimpactada
      JOIN tipoocorrencia tpo ON tpo.id = o.idtipoocorrencia
      JOIN sistemas sis ON sis.id = o.idsistemas;
-    
-insert into STATUS (id, nome) values (1, 'PENDENTE');
-insert into STATUS (id, nome) values (2, 'CONCLUÍDO');
+
+insert into  usuario (id, nome, email, pass) values (1,'TESTE','a@a.com','a');
+  
+insert into  STATUS (id, nome) values (1,'PENDENTE');
+insert into  STATUS (id, nome) values (2,'CONCLUÍDO');
+
+insert into  EMPRESA (id, nome) values (1,'SANTANDER');
+insert into  EMPRESA (id, nome) values (2,'SBK');
+insert into  EMPRESA (id, nome) values (3,'OLÉ');
+insert into  EMPRESA (id, nome) values (4,'ZURICH');
+
+insert into  EQUIPE (id, nome) values (1,'AREAS EXTERNAS');
+insert into  EQUIPE (id, nome) values (2,'ATIVOS');
+insert into  EQUIPE (id, nome) values (3,'CARTÕES');
+insert into  EQUIPE (id, nome) values (4,'CONTA CONRRENTE');
+insert into  EQUIPE (id, nome) values (5,'DEFESA');
+insert into  EQUIPE (id, nome) values (6,'DNRJ');
+insert into  EQUIPE (id, nome) values (7,'FINANCEIRA');
+insert into  EQUIPE (id, nome) values (8,'FRAUDE');
+insert into  EQUIPE (id, nome) values (9,'INTIMAÇÕES');
+insert into  EQUIPE (id, nome) values (10,'MOTOR');
+insert into  EQUIPE (id, nome) values (11,'OFICIOS');
+insert into  EQUIPE (id, nome) values (12,'OLÉ CONSIGNADO');
+insert into  EQUIPE (id, nome) values (13,'ORGÃO REGULADORES');
+insert into  EQUIPE (id, nome) values (14,'OUTROS');
+insert into  EQUIPE (id, nome) values (15,'PROCURA DOCUMENTO');
+insert into  EQUIPE (id, nome) values (16,'RECEPÇÃO');
+insert into  EQUIPE (id, nome) values (17,'TRIAG CADASTRO');
+insert into  EQUIPE (id, nome) values (18,'ZURICH');
+insert into  EQUIPE (id, nome) values (19,'FINANCEIRA/OUTROS');
+
+insert into  TIPOOCORRENCIA (id, nome) values (1,'ERRO');
+insert into  TIPOOCORRENCIA (id, nome) values (2,'FALTA DE REDE');
+insert into  TIPOOCORRENCIA (id, nome) values (3,'FRETADO');
+insert into  TIPOOCORRENCIA (id, nome) values (4,'INFRAESTRUTURA');
+insert into  TIPOOCORRENCIA (id, nome) values (5,'LENTIDÃO');
+insert into  TIPOOCORRENCIA (id, nome) values (6,'PERCA DE ACESSO');
+insert into  TIPOOCORRENCIA (id, nome) values (7,'QUEDA DE ENERGIA');
+insert into  TIPOOCORRENCIA (id, nome) values (8,'QUEDA SISTICA');
+insert into  TIPOOCORRENCIA (id, nome) values (9,'SEM ACESSO');
+
+insert into  SISTEMAS (id, nome) values (1,'BENNER');
+insert into  SISTEMAS (id, nome) values (2,'BOXBOT');
+insert into  SISTEMAS (id, nome) values (3,'CICA');
+insert into  SISTEMAS (id, nome) values (4,'COLDWEB');
+insert into  SISTEMAS (id, nome) values (5,'CRM');
+insert into  SISTEMAS (id, nome) values (6,'CSG');
+insert into  SISTEMAS (id, nome) values (7,'EXYON');
+insert into  SISTEMAS (id, nome) values (8,'GUARDA FISICA');
+insert into  SISTEMAS (id, nome) values (9,'HARDWARE');
+insert into  SISTEMAS (id, nome) values (10,'LINK SANTANDER');
+insert into  SISTEMAS (id, nome) values (11,'LOMBARD');
+insert into  SISTEMAS (id, nome) values (12,'LY');
+insert into  SISTEMAS (id, nome) values (13,'LYNX');
+insert into  SISTEMAS (id, nome) values (14,'MANAGER');
+insert into  SISTEMAS (id, nome) values (15,'NEW SPACE');
+insert into  SISTEMAS (id, nome) values (16,'OUTROS SISTEMAS');
+insert into  SISTEMAS (id, nome) values (17,'PASTA DIGITAL');
+insert into  SISTEMAS (id, nome) values (18,'PEOPLE SOFT');
+insert into  SISTEMAS (id, nome) values (19,'PIM/PIR');
+insert into  SISTEMAS (id, nome) values (20,'PIPEFY');
+insert into  SISTEMAS (id, nome) values (21,'PORTAIS CONSIGNADO');
+insert into  SISTEMAS (id, nome) values (22,'PORTAL AYMORÉ');
+insert into  SISTEMAS (id, nome) values (23,'PORTAL DO CONTRATO');
+insert into  SISTEMAS (id, nome) values (24,'RECTOR');
+insert into  SISTEMAS (id, nome) values (25,'RPA SBK');
+insert into  SISTEMAS (id, nome) values (26,'SERASA/SPC');
+insert into  SISTEMAS (id, nome) values (27,'SINJUD');
+insert into  SISTEMAS (id, nome) values (28,'SISCARD');
+insert into  SISTEMAS (id, nome) values (29,'STOQUE');
+insert into  SISTEMAS (id, nome) values (30,'TFC');
+insert into  SISTEMAS (id, nome) values (31,'TMA');
+insert into  SISTEMAS (id, nome) values (32,'VENTANA');
+insert into  SISTEMAS (id, nome) values (33,'VPN');
+insert into  SISTEMAS (id, nome) values (34,'YK');
+
+insert into  AREAIMPACTADA (id, nome) values (1,'DEFESA');
+insert into  AREAIMPACTADA (id, nome) values (2,'MOTOR');
+insert into  AREAIMPACTADA (id, nome) values (3,'SOJ');
+
+insert into  ETAPAIMPACTADA (id, nome) values (1,'CADASTRO');
+insert into  ETAPAIMPACTADA (id, nome) values (2,'COMPLEMENTO');
+insert into  ETAPAIMPACTADA (id, nome) values (3,'CONCLUSÃO');
+insert into  ETAPAIMPACTADA (id, nome) values (4,'RECEPÇÃO');
+insert into  ETAPAIMPACTADA (id, nome) values (5,'TRIAGEM');
